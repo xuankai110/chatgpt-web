d601a949885f3744e89d1af58e029ce3ac73875c
汪勇 <12698663+wang-xuan-kai@user.noreply.gitee.com>
2023-04-21 17:15:22 +0800

    0421 public

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: '梅西',
+      description: 'VIP 尊享 <a href="http://1.12.223.60/login" class="text-blue-500" target="_blank" >球米球</a>',
     },
   }
 }
